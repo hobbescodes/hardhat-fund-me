6e2f15ce1a7064579e0fe40f7c7e587ed9cde12d
hobbescodes <jakob.hauble@gmail.com>
2022-06-28 08:42:42 -0500

    add fund and withdraw scripts

---
+++ scripts/fund.js
@@ -0,0 +1,20 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+
+const main = async () => {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContract("FundMe", deployer)
+    console.log(`Got contract FundMe at ${fundMe.address}`)
+    console.log("Funding contract...")
+    const transactionResponse = await fundMe.fund({
+        value: ethers.utils.parseEther("1"),
+    })
+    await transactionResponse.wait()
+    console.log("Funded!")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
