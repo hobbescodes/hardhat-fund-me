c0484b225a37057171520b39cacc0f3baa5de1df
hobbescodes <jakob.hauble@gmail.com>
2022-06-27 22:49:50 -0500

    update deploy fund me script

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,16 +1,28 @@
 const { network } = require("hardhat")
-const { networkConfig } = require("../helper-hardhat-config")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
-    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    log("-------------------------------------------------")
+    log("Deploying FundMe contract and waiting for confirmations...")
 
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [],
+        args: [ethUsdPriceFeedAddress],
         log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
 }
+
+module.exports.tags = ["all", "fundme"]
