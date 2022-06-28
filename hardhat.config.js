d21619332d26282a0084a4a56f09ccebb17498e8
hobbescodes <jakob.hauble@gmail.com>
2022-06-27 23:15:11 -0500

    add programmatic verification script

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,16 +6,6 @@ require("hardhat-gas-reporter")
 require("solidity-coverage")
 require("hardhat-deploy")
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners()
-
-    for (const account of accounts) {
-        console.log(account.address)
-    }
-})
-
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const KOVAN_RPC_URL =
     process.env.KOVAN_RPC_URL ||
@@ -28,10 +18,6 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
 module.exports = {
     defaultNetwork: "hardhat",
-    // solidity: "0.8.8",
-    solidity: {
-        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
-    },
     networks: {
         hardhat: {
             chainId: 31337,
@@ -50,20 +36,30 @@ module.exports = {
             blockConfirmations: 6,
         },
     },
-    gasReporter: {
-        enabled: process.env.REPORT_GAS !== undefined,
-        currency: "USD",
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.6.6",
+            },
+        ],
     },
     etherscan: {
-        apiKey: process.env.ETHERSCAN_API_KEY,
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: COINMARKETCAP_API_KEY,
     },
     namedAccounts: {
         deployer: {
-            default: 0,
-            4: 1,
-        },
-        users: {
-            default: 1,
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
     },
 }
