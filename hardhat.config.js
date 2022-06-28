372720b5007b392572b74095b028058cdb0d777a
hobbescodes <jakob.hauble@gmail.com>
2022-06-27 22:33:55 -0500

    update contracts, add mock aggregator, add deploy mocks script

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require("@nomiclabs/hardhat-etherscan")
 require("@nomiclabs/hardhat-waffle")
 require("hardhat-gas-reporter")
 require("solidity-coverage")
+require("hardhat-deploy")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -15,21 +16,38 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     }
 })
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const KOVAN_RPC_URL =
+    process.env.KOVAN_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const RINKEBY_RPC_URL =
+    process.env.RINKEBY_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xsoegasrgartasrteas"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-    solidity: "0.8.8",
+    defaultNetwork: "hardhat",
+    // solidity: "0.8.8",
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
     networks: {
-        ropsten: {
-            url: process.env.ROPSTEN_URL || "",
-            accounts:
-                process.env.PRIVATE_KEY !== undefined
-                    ? [process.env.PRIVATE_KEY]
-                    : [],
+        hardhat: {
+            chainId: 31337,
+            // gasPrice: 130000000000,
+        },
+        kovan: {
+            url: KOVAN_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 42,
+            blockConfirmations: 6,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
         },
     },
     gasReporter: {
@@ -38,5 +56,14 @@ module.exports = {
     },
     etherscan: {
         apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            4: 1,
+        },
+        users: {
+            default: 1,
+        },
     },
 }
